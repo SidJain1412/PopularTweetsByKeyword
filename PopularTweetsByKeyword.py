d83974dbe2c36640cc86c46acc7d878cc71b4842
Siddharth Jain <siddharth.y.jain@oracle.com>
2019-01-04 10:52:18 +0530

    Cleaned up the code, taking keyword as input now

--- PopularTweetsByKeyword.py
+++ PopularTweetsByKeyword.py
@@ -1,20 +1,27 @@
+import tweepy
+from tweepy import OAuthHandler
+
+
 def popular(keyword):
-    #importing tweepy to connect to twitter
-    import tweepy
-    from tweepy import OAuthHandler
-    #from twitter app id
-    consumer_key=""
-    consumer_secret=""
-    access_token=""
-    access_secret=""
-    #initializing contact point (api variable)
-    auth=OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token,access_secret)
-    api=tweepy.API(auth)
-    
-    for tech in api.search(keyword,result_type="popular",tweet_mode="extended"):
-        print(tech.user.screen_name,end="")  #to print more than 140 chars, use 
-        if(tech.user.verified):              #tweet_mode=extended and .full_text
-            print(" ✓: ",end="")
-        print(tech.full_text)        
-        print()                    
+    # from twitter app id
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_secret = ""
+    # initializing contact point (api variable)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+
+    for tech in api.search(keyword, result_type="popular", tweet_mode="extended"):
+        # to print more than 140 chars, use tweet_mode=extended and .full_text
+        print(tech.user.screen_name, end="")
+        if(tech.user.verified):
+            print(" ✓: ", end="")
+        print(tech.full_text)
+        print()
+
+
+if __name__ == "__main__":
+    keyword = input("Please enter the keyword to search for: ")
+    popular(keyword)
